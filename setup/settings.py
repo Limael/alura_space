3cedbaf03b0d60b83f387df96943152dc8e3fde0
Limael <limagustavorafael1@gmail.com>
2024-08-15 09:12:28 -0300

    base made

--- setup/settings.py
+++ setup/settings.py
@@ -119,7 +119,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'setup/static')
+]
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
